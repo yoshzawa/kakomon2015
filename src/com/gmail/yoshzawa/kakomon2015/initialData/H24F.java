0a5ad52778963dc8778bb59d3954bbf943917376
yoshzawa <yoshzawa@gmail.com>
2015-09-05 14:42:23 +0900

    COMMITに対象ファイルを入れてなかった

---
+++ src/com/gmail/yoshzawa/kakomon2015/initialData/H24F.java
@@ -0,0 +1,185 @@
+package com.gmail.yoshzawa.kakomon2015.initialData;
+
+import com.gmail.yoshzawa.kakomon2015.dataStore.Mondai;
+import com.gmail.yoshzawa.kakomon2015.dataStore.Question;
+
+public class H24F {
+	public static void m() throws NoSuchFieldException {
+		new Mondai("FE-H24A-AM-01", "FE-H24A-AM-01", "FE-H24A-AM", 1,"H24FFEAM01").put();
+		new Mondai("FE-H24A-AM-02", "FE-H24A-AM-02", "FE-H24A-AM", 2,"H24FFEAM02").put();
+		new Mondai("FE-H24A-AM-03", "FE-H24A-AM-03", "FE-H24A-AM", 3,"H24FFEAM03").put();
+		new Mondai("FE-H24A-AM-04", "FE-H24A-AM-04", "FE-H24A-AM", 4,"H24FFEAM04").put();
+		new Mondai("FE-H24A-AM-05", "FE-H24A-AM-05", "FE-H24A-AM", 5,"H24FFEAM05").put();
+		new Mondai("FE-H24A-AM-06", "FE-H24A-AM-06", "FE-H24A-AM", 6,"H24FFEAM06").put();
+		new Mondai("FE-H24A-AM-07", "FE-H24A-AM-07", "FE-H24A-AM", 7,"H24FFEAM07").put();
+		new Mondai("FE-H24A-AM-08", "FE-H24A-AM-08", "FE-H24A-AM", 8,"H24FFEAM08").put();
+		new Mondai("FE-H24A-AM-09", "FE-H24A-AM-09", "FE-H24A-AM", 9,"H24FFEAM09").put();
+		new Mondai("FE-H24A-AM-10", "FE-H24A-AM-10", "FE-H24A-AM", 10,"H24FFEAM10").put();
+		
+		new Mondai("FE-H24A-AM-11", "FE-H24A-AM-11", "FE-H24A-AM", 11,"H24FFEAM11").put();
+		new Mondai("FE-H24A-AM-12", "FE-H24A-AM-12", "FE-H24A-AM", 12,"H24FFEAM12").put();
+		new Mondai("FE-H24A-AM-13", "FE-H24A-AM-13", "FE-H24A-AM", 13,"H24FFEAM13").put();
+		new Mondai("FE-H24A-AM-14", "FE-H24A-AM-14", "FE-H24A-AM", 14,"H24FFEAM14").put();
+		new Mondai("FE-H24A-AM-15", "FE-H24A-AM-15", "FE-H24A-AM", 15,"H24FFEAM15").put();
+		new Mondai("FE-H24A-AM-16", "FE-H24A-AM-16", "FE-H24A-AM", 16,"H24FFEAM16").put();
+		new Mondai("FE-H24A-AM-17", "FE-H24A-AM-17", "FE-H24A-AM", 17,"H24FFEAM17").put();
+		new Mondai("FE-H24A-AM-18", "FE-H24A-AM-18", "FE-H24A-AM", 18,"H24FFEAM18").put();
+		new Mondai("FE-H24A-AM-19", "FE-H24A-AM-19", "FE-H24A-AM", 19,"H24FFEAM19").put();
+		new Mondai("FE-H24A-AM-20", "FE-H24A-AM-20", "FE-H24A-AM", 20,"H24FFEAM20").put();
+
+		new Mondai("FE-H24A-AM-21", "FE-H24A-AM-21", "FE-H24A-AM", 21,"H24FFEAM21").put();
+		new Mondai("FE-H24A-AM-22", "FE-H24A-AM-22", "FE-H24A-AM", 22,"H24FFEAM22").put();
+		new Mondai("FE-H24A-AM-23", "FE-H24A-AM-23", "FE-H24A-AM", 23,"H24FFEAM23").put();
+		new Mondai("FE-H24A-AM-24", "FE-H24A-AM-24", "FE-H24A-AM", 24,"H24FFEAM24").put();
+		new Mondai("FE-H24A-AM-25", "FE-H24A-AM-25", "FE-H24A-AM", 25,"H24FFEAM25").put();
+		new Mondai("FE-H24A-AM-26", "FE-H24A-AM-26", "FE-H24A-AM", 26,"H24FFEAM26").put();
+		new Mondai("FE-H24A-AM-27", "FE-H24A-AM-27", "FE-H24A-AM", 27,"H24FFEAM27").put();
+		new Mondai("FE-H24A-AM-28", "FE-H24A-AM-28", "FE-H24A-AM", 28,"H24FFEAM28").put();
+		new Mondai("FE-H24A-AM-29", "FE-H24A-AM-29", "FE-H24A-AM", 29,"H24FFEAM29").put();
+		new Mondai("FE-H24A-AM-30", "FE-H24A-AM-30", "FE-H24A-AM", 30,"H24FFEAM30").put();
+
+		new Mondai("FE-H24A-AM-31", "FE-H24A-AM-31", "FE-H24A-AM", 31,"H24FFEAM31").put();
+		new Mondai("FE-H24A-AM-32", "FE-H24A-AM-32", "FE-H24A-AM", 32,"H24FFEAM32").put();
+		new Mondai("FE-H24A-AM-33", "FE-H24A-AM-33", "FE-H24A-AM", 33,"H24FFEAM33").put();
+		new Mondai("FE-H24A-AM-34", "FE-H24A-AM-34", "FE-H24A-AM", 34,"H24FFEAM34").put();
+		new Mondai("FE-H24A-AM-35", "FE-H24A-AM-35", "FE-H24A-AM", 35,"H24FFEAM35").put();
+		new Mondai("FE-H24A-AM-36", "FE-H24A-AM-36", "FE-H24A-AM", 36,"H24FFEAM36").put();
+		new Mondai("FE-H24A-AM-37", "FE-H24A-AM-37", "FE-H24A-AM", 37,"H24FFEAM37").put();
+		new Mondai("FE-H24A-AM-38", "FE-H24A-AM-38", "FE-H24A-AM", 38,"H24FFEAM38").put();
+		new Mondai("FE-H24A-AM-39", "FE-H24A-AM-39", "FE-H24A-AM", 39,"H24FFEAM39").put();
+		new Mondai("FE-H24A-AM-40", "FE-H24A-AM-40", "FE-H24A-AM", 40,"H24FFEAM40").put();
+
+		new Mondai("FE-H24A-AM-41", "FE-H24A-AM-41", "FE-H24A-AM", 41,"H24FFEAM41").put();
+		new Mondai("FE-H24A-AM-42", "FE-H24A-AM-42", "FE-H24A-AM", 42,"H24FFEAM42").put();
+		new Mondai("FE-H24A-AM-43", "FE-H24A-AM-43", "FE-H24A-AM", 43,"H24FFEAM43").put();
+		new Mondai("FE-H24A-AM-44", "FE-H24A-AM-44", "FE-H24A-AM", 44,"H24FFEAM44").put();
+		new Mondai("FE-H24A-AM-45", "FE-H24A-AM-45", "FE-H24A-AM", 45,"H24FFEAM45").put();
+		new Mondai("FE-H24A-AM-46", "FE-H24A-AM-46", "FE-H24A-AM", 46,"H24FFEAM46").put();
+		new Mondai("FE-H24A-AM-47", "FE-H24A-AM-47", "FE-H24A-AM", 47,"H24FFEAM47").put();
+		new Mondai("FE-H24A-AM-48", "FE-H24A-AM-48", "FE-H24A-AM", 48,"H24FFEAM48").put();
+		new Mondai("FE-H24A-AM-49", "FE-H24A-AM-49", "FE-H24A-AM", 49,"H24FFEAM49").put();
+		new Mondai("FE-H24A-AM-50", "FE-H24A-AM-50", "FE-H24A-AM", 50,"H24FFEAM50").put();
+
+		new Mondai("FE-H24A-AM-51", "FE-H24A-AM-51", "FE-H24A-AM", 51,"H24FFEAM51").put();
+		new Mondai("FE-H24A-AM-52", "FE-H24A-AM-52", "FE-H24A-AM", 52,"H24FFEAM52").put();
+		new Mondai("FE-H24A-AM-53", "FE-H24A-AM-53", "FE-H24A-AM", 53,"H24FFEAM53").put();
+		new Mondai("FE-H24A-AM-54", "FE-H24A-AM-54", "FE-H24A-AM", 54,"H24FFEAM54").put();
+		new Mondai("FE-H24A-AM-55", "FE-H24A-AM-55", "FE-H24A-AM", 55,"H24FFEAM55").put();
+		new Mondai("FE-H24A-AM-56", "FE-H24A-AM-56", "FE-H24A-AM", 56,"H24FFEAM56").put();
+		new Mondai("FE-H24A-AM-57", "FE-H24A-AM-57", "FE-H24A-AM", 57,"H24FFEAM57").put();
+		new Mondai("FE-H24A-AM-58", "FE-H24A-AM-58", "FE-H24A-AM", 58,"H24FFEAM58").put();
+		new Mondai("FE-H24A-AM-59", "FE-H24A-AM-59", "FE-H24A-AM", 59,"H24FFEAM59").put();
+		new Mondai("FE-H24A-AM-60", "FE-H24A-AM-60", "FE-H24A-AM", 60,"H24FFEAM60").put();
+
+		new Mondai("FE-H24A-AM-61", "FE-H24A-AM-61", "FE-H24A-AM", 61,"H24FFEAM61").put();
+		new Mondai("FE-H24A-AM-62", "FE-H24A-AM-62", "FE-H24A-AM", 62,"H24FFEAM62").put();
+		new Mondai("FE-H24A-AM-63", "FE-H24A-AM-63", "FE-H24A-AM", 63,"H24FFEAM63").put();
+		new Mondai("FE-H24A-AM-64", "FE-H24A-AM-64", "FE-H24A-AM", 64,"H24FFEAM64").put();
+		new Mondai("FE-H24A-AM-65", "FE-H24A-AM-65", "FE-H24A-AM", 65,"H24FFEAM65").put();
+		new Mondai("FE-H24A-AM-66", "FE-H24A-AM-66", "FE-H24A-AM", 66,"H24FFEAM66").put();
+		new Mondai("FE-H24A-AM-67", "FE-H24A-AM-67", "FE-H24A-AM", 67,"H24FFEAM67").put();
+		new Mondai("FE-H24A-AM-68", "FE-H24A-AM-68", "FE-H24A-AM", 68,"H24FFEAM68").put();
+		new Mondai("FE-H24A-AM-69", "FE-H24A-AM-69", "FE-H24A-AM", 69,"H24FFEAM69").put();
+		new Mondai("FE-H24A-AM-70", "FE-H24A-AM-70", "FE-H24A-AM", 70,"H24FFEAM70").put();
+
+		new Mondai("FE-H24A-AM-71", "FE-H24A-AM-71", "FE-H24A-AM", 71,"H24FFEAM71").put();
+		new Mondai("FE-H24A-AM-72", "FE-H24A-AM-72", "FE-H24A-AM", 72,"H24FFEAM72").put();
+		new Mondai("FE-H24A-AM-73", "FE-H24A-AM-73", "FE-H24A-AM", 73,"H24FFEAM73").put();
+		new Mondai("FE-H24A-AM-74", "FE-H24A-AM-74", "FE-H24A-AM", 74,"H24FFEAM74").put();
+		new Mondai("FE-H24A-AM-75", "FE-H24A-AM-75", "FE-H24A-AM", 75,"H24FFEAM75").put();
+		new Mondai("FE-H24A-AM-76", "FE-H24A-AM-76", "FE-H24A-AM", 76,"H24FFEAM76").put();
+		new Mondai("FE-H24A-AM-77", "FE-H24A-AM-77", "FE-H24A-AM", 77,"H24FFEAM77").put();
+		new Mondai("FE-H24A-AM-78", "FE-H24A-AM-78", "FE-H24A-AM", 78,"H24FFEAM78").put();
+		new Mondai("FE-H24A-AM-79", "FE-H24A-AM-79", "FE-H24A-AM", 79,"H24FFEAM79").put();
+		new Mondai("FE-H24A-AM-80", "FE-H24A-AM-80", "FE-H24A-AM", 80,"H24FFEAM80").put();
+		}
+	public static void q() throws NoSuchFieldException {
+		new Question("H24FFEAM01", "平成24年秋 基本情報 午前 01", 4, 3).put();
+		new Question("H24FFEAM02", "平成24年秋 基本情報 午前 02", 4, 2).put();
+		new Question("H24FFEAM03", "平成24年秋 基本情報 午前 03", 4, 1).put();
+		new Question("H24FFEAM04", "平成24年秋 基本情報 午前 04", 4, 2).put();
+		new Question("H24FFEAM05", "平成24年秋 基本情報 午前 05", 4, 2).put();
+		new Question("H24FFEAM06", "平成24年秋 基本情報 午前 06", 4, 2).put();
+		new Question("H24FFEAM07", "平成24年秋 基本情報 午前 07", 4, 2).put();
+		new Question("H24FFEAM08", "平成24年秋 基本情報 午前 08", 4, 2).put();
+		new Question("H24FFEAM09", "平成24年秋 基本情報 午前 09", 4, 3).put();
+		new Question("H24FFEAM10", "平成24年秋 基本情報 午前 10", 4, 4).put();
+
+		new Question("H24FFEAM11", "平成24年秋 基本情報 午前 11", 4, 3).put();
+		new Question("H24FFEAM12", "平成24年秋 基本情報 午前 12", 4, 4).put();
+		new Question("H24FFEAM13", "平成24年秋 基本情報 午前 13", 4, 3).put();
+		new Question("H24FFEAM14", "平成24年秋 基本情報 午前 14", 4, 1).put();
+		new Question("H24FFEAM15", "平成24年秋 基本情報 午前 15", 4, 4).put();
+		new Question("H24FFEAM16", "平成24年秋 基本情報 午前 16", 4, 4).put();
+		new Question("H24FFEAM17", "平成24年秋 基本情報 午前 17", 4, 3).put();
+		new Question("H24FFEAM18", "平成24年秋 基本情報 午前 18", 4, 3).put();
+		new Question("H24FFEAM19", "平成24年秋 基本情報 午前 19", 4, 4).put();
+		new Question("H24FFEAM20", "平成24年秋 基本情報 午前 20", 4, 4).put();
+
+		new Question("H24FFEAM21", "平成24年秋 基本情報 午前 21", 4, 1).put();
+		new Question("H24FFEAM22", "平成24年秋 基本情報 午前 22", 4, 2).put();
+		new Question("H24FFEAM23", "平成24年秋 基本情報 午前 23", 4, 4).put();
+		new Question("H24FFEAM24", "平成24年秋 基本情報 午前 24", 4, 2).put();
+		new Question("H24FFEAM25", "平成24年秋 基本情報 午前 25", 4, 4).put();
+		new Question("H24FFEAM26", "平成24年秋 基本情報 午前 26", 4, 1).put();
+		new Question("H24FFEAM27", "平成24年秋 基本情報 午前 27", 4, 2).put();
+		new Question("H24FFEAM28", "平成24年秋 基本情報 午前 28", 4, 2).put();
+		new Question("H24FFEAM29", "平成24年秋 基本情報 午前 29", 4, 1).put();
+		new Question("H24FFEAM30", "平成24年秋 基本情報 午前 30", 4, 4).put();
+		
+		new Question("H24FFEAM31", "平成24年秋 基本情報 午前 31", 4, 3).put();
+		new Question("H24FFEAM32", "平成24年秋 基本情報 午前 32", 4, 3).put();
+		new Question("H24FFEAM33", "平成24年秋 基本情報 午前 33", 4, 4).put();
+		new Question("H24FFEAM34", "平成24年秋 基本情報 午前 34", 4, 4).put();
+		new Question("H24FFEAM35", "平成24年秋 基本情報 午前 35", 4, 2).put();
+		new Question("H24FFEAM36", "平成24年秋 基本情報 午前 36", 4, 2).put();
+		new Question("H24FFEAM37", "平成24年秋 基本情報 午前 37", 4, 2).put();
+		new Question("H24FFEAM38", "平成24年秋 基本情報 午前 38", 4, 3).put();
+		new Question("H24FFEAM39", "平成24年秋 基本情報 午前 39", 4, 2).put();
+		new Question("H24FFEAM40", "平成24年秋 基本情報 午前 40", 4, 1).put();
+		
+		new Question("H24FFEAM41", "平成24年秋 基本情報 午前 41", 4, 2).put();
+		new Question("H24FFEAM42", "平成24年秋 基本情報 午前 42", 4, 2).put();
+		new Question("H24FFEAM43", "平成24年秋 基本情報 午前 43", 4, 1).put();
+		new Question("H24FFEAM44", "平成24年秋 基本情報 午前 44", 4, 3).put();
+		new Question("H24FFEAM45", "平成24年秋 基本情報 午前 45", 4, 4).put();
+		new Question("H24FFEAM46", "平成24年秋 基本情報 午前 46", 4, 2).put();
+		new Question("H24FFEAM47", "平成24年秋 基本情報 午前 47", 4, 4).put();
+		new Question("H24FFEAM48", "平成24年秋 基本情報 午前 48", 4, 2).put();
+		new Question("H24FFEAM49", "平成24年秋 基本情報 午前 49", 4, 4).put();
+		new Question("H24FFEAM50", "平成24年秋 基本情報 午前 50", 4, 1).put();
+	
+		new Question("H24FFEAM51", "平成24年秋 基本情報 午前 51", 4, 2).put();
+		new Question("H24FFEAM52", "平成24年秋 基本情報 午前 52", 4, 4).put();
+		new Question("H24FFEAM53", "平成24年秋 基本情報 午前 53", 4, 4).put();
+		new Question("H24FFEAM54", "平成24年秋 基本情報 午前 54", 4, 1).put();
+		new Question("H24FFEAM55", "平成24年秋 基本情報 午前 55", 4, 3).put();
+		new Question("H24FFEAM56", "平成24年秋 基本情報 午前 56", 4, 3).put();
+		new Question("H24FFEAM57", "平成24年秋 基本情報 午前 57", 4, 2).put();
+		new Question("H24FFEAM58", "平成24年秋 基本情報 午前 58", 4, 4).put();
+		new Question("H24FFEAM59", "平成24年秋 基本情報 午前 59", 4, 2).put();
+		new Question("H24FFEAM60", "平成24年秋 基本情報 午前 60", 4, 1).put();
+		
+		new Question("H24FFEAM61", "平成24年秋 基本情報 午前 61", 4, 3).put();
+		new Question("H24FFEAM62", "平成24年秋 基本情報 午前 62", 4, 2).put();
+		new Question("H24FFEAM63", "平成24年秋 基本情報 午前 63", 4, 1).put();
+		new Question("H24FFEAM64", "平成24年秋 基本情報 午前 64", 4, 1).put();
+		new Question("H24FFEAM65", "平成24年秋 基本情報 午前 65", 4, 1).put();
+		new Question("H24FFEAM66", "平成24年秋 基本情報 午前 66", 4, 3).put();
+		new Question("H24FFEAM67", "平成24年秋 基本情報 午前 67", 4, 3).put();
+		new Question("H24FFEAM68", "平成24年秋 基本情報 午前 68", 4, 4).put();
+		new Question("H24FFEAM69", "平成24年秋 基本情報 午前 69", 4, 1).put();
+		new Question("H24FFEAM70", "平成24年秋 基本情報 午前 70", 4, 1).put();
+		
+		new Question("H24FFEAM71", "平成24年秋 基本情報 午前 71", 4, 1).put();
+		new Question("H24FFEAM72", "平成24年秋 基本情報 午前 72", 4, 4).put();
+		new Question("H24FFEAM73", "平成24年秋 基本情報 午前 73", 4, 3).put();
+		new Question("H24FFEAM74", "平成24年秋 基本情報 午前 74", 4, 4).put();
+		new Question("H24FFEAM75", "平成24年秋 基本情報 午前 75", 4, 3).put();
+		new Question("H24FFEAM76", "平成24年秋 基本情報 午前 76", 4, 3).put();
+		new Question("H24FFEAM77", "平成24年秋 基本情報 午前 77", 4, 2).put();
+		new Question("H24FFEAM78", "平成24年秋 基本情報 午前 78", 4, 2).put();
+		new Question("H24FFEAM79", "平成24年秋 基本情報 午前 79", 4, 2).put();
+		new Question("H24FFEAM80", "平成24年秋 基本情報 午前 80", 4, 3).put();
+	}
+}
---
+++ src/com/gmail/yoshzawa/kakomon2015/initialData/H24S.java
@@ -0,0 +1,185 @@
+package com.gmail.yoshzawa.kakomon2015.initialData;
+
+import com.gmail.yoshzawa.kakomon2015.dataStore.Mondai;
+import com.gmail.yoshzawa.kakomon2015.dataStore.Question;
+
+public class H24S {
+	public static void m() throws NoSuchFieldException {
+		new Mondai("FE-H24H-AM-01", "FE-H24H-AM-01", "FE-H24H-AM", 1,"H24SFEAM01").put();
+		new Mondai("FE-H24H-AM-02", "FE-H24H-AM-02", "FE-H24H-AM", 2,"H24SFEAM02").put();
+		new Mondai("FE-H24H-AM-03", "FE-H24H-AM-03", "FE-H24H-AM", 3,"H24SFEAM03").put();
+		new Mondai("FE-H24H-AM-04", "FE-H24H-AM-04", "FE-H24H-AM", 4,"H24SFEAM04").put();
+		new Mondai("FE-H24H-AM-05", "FE-H24H-AM-05", "FE-H24H-AM", 5,"H24SFEAM05").put();
+		new Mondai("FE-H24H-AM-06", "FE-H24H-AM-06", "FE-H24H-AM", 6,"H24SFEAM06").put();
+		new Mondai("FE-H24H-AM-07", "FE-H24H-AM-07", "FE-H24H-AM", 7,"H24SFEAM07").put();
+		new Mondai("FE-H24H-AM-08", "FE-H24H-AM-08", "FE-H24H-AM", 8,"H24SFEAM08").put();
+		new Mondai("FE-H24H-AM-09", "FE-H24H-AM-09", "FE-H24H-AM", 9,"H24SFEAM09").put();
+		new Mondai("FE-H24H-AM-10", "FE-H24H-AM-10", "FE-H24H-AM", 10,"H24SFEAM10").put();
+		
+		new Mondai("FE-H24H-AM-11", "FE-H24H-AM-11", "FE-H24H-AM", 11,"H24SFEAM11").put();
+		new Mondai("FE-H24H-AM-12", "FE-H24H-AM-12", "FE-H24H-AM", 12,"H24SFEAM12").put();
+		new Mondai("FE-H24H-AM-13", "FE-H24H-AM-13", "FE-H24H-AM", 13,"H24SFEAM13").put();
+		new Mondai("FE-H24H-AM-14", "FE-H24H-AM-14", "FE-H24H-AM", 14,"H24SFEAM14").put();
+		new Mondai("FE-H24H-AM-15", "FE-H24H-AM-15", "FE-H24H-AM", 15,"H24SFEAM15").put();
+		new Mondai("FE-H24H-AM-16", "FE-H24H-AM-16", "FE-H24H-AM", 16,"H24SFEAM16").put();
+		new Mondai("FE-H24H-AM-17", "FE-H24H-AM-17", "FE-H24H-AM", 17,"H24SFEAM17").put();
+		new Mondai("FE-H24H-AM-18", "FE-H24H-AM-18", "FE-H24H-AM", 18,"H24SFEAM18").put();
+		new Mondai("FE-H24H-AM-19", "FE-H24H-AM-19", "FE-H24H-AM", 19,"H24SFEAM19").put();
+		new Mondai("FE-H24H-AM-20", "FE-H24H-AM-20", "FE-H24H-AM", 20,"H24SFEAM20").put();
+
+		new Mondai("FE-H24H-AM-21", "FE-H24H-AM-21", "FE-H24H-AM", 21,"H24SFEAM21").put();
+		new Mondai("FE-H24H-AM-22", "FE-H24H-AM-22", "FE-H24H-AM", 22,"H24SFEAM22").put();
+		new Mondai("FE-H24H-AM-23", "FE-H24H-AM-23", "FE-H24H-AM", 23,"H24SFEAM23").put();
+		new Mondai("FE-H24H-AM-24", "FE-H24H-AM-24", "FE-H24H-AM", 24,"H24SFEAM24").put();
+		new Mondai("FE-H24H-AM-25", "FE-H24H-AM-25", "FE-H24H-AM", 25,"H24SFEAM25").put();
+		new Mondai("FE-H24H-AM-26", "FE-H24H-AM-26", "FE-H24H-AM", 26,"H24SFEAM26").put();
+		new Mondai("FE-H24H-AM-27", "FE-H24H-AM-27", "FE-H24H-AM", 27,"H24SFEAM27").put();
+		new Mondai("FE-H24H-AM-28", "FE-H24H-AM-28", "FE-H24H-AM", 28,"H24SFEAM28").put();
+		new Mondai("FE-H24H-AM-29", "FE-H24H-AM-29", "FE-H24H-AM", 29,"H24SFEAM29").put();
+		new Mondai("FE-H24H-AM-30", "FE-H24H-AM-30", "FE-H24H-AM", 30,"H24SFEAM30").put();
+
+		new Mondai("FE-H24H-AM-31", "FE-H24H-AM-31", "FE-H24H-AM", 31,"H24SFEAM31").put();
+		new Mondai("FE-H24H-AM-32", "FE-H24H-AM-32", "FE-H24H-AM", 32,"H24SFEAM32").put();
+		new Mondai("FE-H24H-AM-33", "FE-H24H-AM-33", "FE-H24H-AM", 33,"H24SFEAM33").put();
+		new Mondai("FE-H24H-AM-34", "FE-H24H-AM-34", "FE-H24H-AM", 34,"H24SFEAM34").put();
+		new Mondai("FE-H24H-AM-35", "FE-H24H-AM-35", "FE-H24H-AM", 35,"H24SFEAM35").put();
+		new Mondai("FE-H24H-AM-36", "FE-H24H-AM-36", "FE-H24H-AM", 36,"H24SFEAM36").put();
+		new Mondai("FE-H24H-AM-37", "FE-H24H-AM-37", "FE-H24H-AM", 37,"H24SFEAM37").put();
+		new Mondai("FE-H24H-AM-38", "FE-H24H-AM-38", "FE-H24H-AM", 38,"H24SFEAM38").put();
+		new Mondai("FE-H24H-AM-39", "FE-H24H-AM-39", "FE-H24H-AM", 39,"H24SFEAM39").put();
+		new Mondai("FE-H24H-AM-40", "FE-H24H-AM-40", "FE-H24H-AM", 40,"H24SFEAM40").put();
+
+		new Mondai("FE-H24H-AM-41", "FE-H24H-AM-41", "FE-H24H-AM", 41,"H24SFEAM41").put();
+		new Mondai("FE-H24H-AM-42", "FE-H24H-AM-42", "FE-H24H-AM", 42,"H24SFEAM42").put();
+		new Mondai("FE-H24H-AM-43", "FE-H24H-AM-43", "FE-H24H-AM", 43,"H24SFEAM43").put();
+		new Mondai("FE-H24H-AM-44", "FE-H24H-AM-44", "FE-H24H-AM", 44,"H24SFEAM44").put();
+		new Mondai("FE-H24H-AM-45", "FE-H24H-AM-45", "FE-H24H-AM", 45,"H24SFEAM45").put();
+		new Mondai("FE-H24H-AM-46", "FE-H24H-AM-46", "FE-H24H-AM", 46,"H24SFEAM46").put();
+		new Mondai("FE-H24H-AM-47", "FE-H24H-AM-47", "FE-H24H-AM", 47,"H24SFEAM47").put();
+		new Mondai("FE-H24H-AM-48", "FE-H24H-AM-48", "FE-H24H-AM", 48,"H24SFEAM48").put();
+		new Mondai("FE-H24H-AM-49", "FE-H24H-AM-49", "FE-H24H-AM", 49,"H24SFEAM49").put();
+		new Mondai("FE-H24H-AM-50", "FE-H24H-AM-50", "FE-H24H-AM", 50,"H24SFEAM50").put();
+
+		new Mondai("FE-H24H-AM-51", "FE-H24H-AM-51", "FE-H24H-AM", 51,"H24SFEAM51").put();
+		new Mondai("FE-H24H-AM-52", "FE-H24H-AM-52", "FE-H24H-AM", 52,"H24SFEAM52").put();
+		new Mondai("FE-H24H-AM-53", "FE-H24H-AM-53", "FE-H24H-AM", 53,"H24SFEAM53").put();
+		new Mondai("FE-H24H-AM-54", "FE-H24H-AM-54", "FE-H24H-AM", 54,"H24SFEAM54").put();
+		new Mondai("FE-H24H-AM-55", "FE-H24H-AM-55", "FE-H24H-AM", 55,"H24SFEAM55").put();
+		new Mondai("FE-H24H-AM-56", "FE-H24H-AM-56", "FE-H24H-AM", 56,"H24SFEAM56").put();
+		new Mondai("FE-H24H-AM-57", "FE-H24H-AM-57", "FE-H24H-AM", 57,"H24SFEAM57").put();
+		new Mondai("FE-H24H-AM-58", "FE-H24H-AM-58", "FE-H24H-AM", 58,"H24SFEAM58").put();
+		new Mondai("FE-H24H-AM-59", "FE-H24H-AM-59", "FE-H24H-AM", 59,"H24SFEAM59").put();
+		new Mondai("FE-H24H-AM-60", "FE-H24H-AM-60", "FE-H24H-AM", 60,"H24SFEAM60").put();
+
+		new Mondai("FE-H24H-AM-61", "FE-H24H-AM-61", "FE-H24H-AM", 61,"H24SFEAM61").put();
+		new Mondai("FE-H24H-AM-62", "FE-H24H-AM-62", "FE-H24H-AM", 62,"H24SFEAM62").put();
+		new Mondai("FE-H24H-AM-63", "FE-H24H-AM-63", "FE-H24H-AM", 63,"H24SFEAM63").put();
+		new Mondai("FE-H24H-AM-64", "FE-H24H-AM-64", "FE-H24H-AM", 64,"H24SFEAM64").put();
+		new Mondai("FE-H24H-AM-65", "FE-H24H-AM-65", "FE-H24H-AM", 65,"H24SFEAM65").put();
+		new Mondai("FE-H24H-AM-66", "FE-H24H-AM-66", "FE-H24H-AM", 66,"H24SFEAM66").put();
+		new Mondai("FE-H24H-AM-67", "FE-H24H-AM-67", "FE-H24H-AM", 67,"H24SFEAM67").put();
+		new Mondai("FE-H24H-AM-68", "FE-H24H-AM-68", "FE-H24H-AM", 68,"H24SFEAM68").put();
+		new Mondai("FE-H24H-AM-69", "FE-H24H-AM-69", "FE-H24H-AM", 69,"H24SFEAM69").put();
+		new Mondai("FE-H24H-AM-70", "FE-H24H-AM-70", "FE-H24H-AM", 70,"H24SFEAM70").put();
+
+		new Mondai("FE-H24H-AM-71", "FE-H24H-AM-71", "FE-H24H-AM", 71,"H24SFEAM71").put();
+		new Mondai("FE-H24H-AM-72", "FE-H24H-AM-72", "FE-H24H-AM", 72,"H24SFEAM72").put();
+		new Mondai("FE-H24H-AM-73", "FE-H24H-AM-73", "FE-H24H-AM", 73,"H24SFEAM73").put();
+		new Mondai("FE-H24H-AM-74", "FE-H24H-AM-74", "FE-H24H-AM", 74,"H24SFEAM74").put();
+		new Mondai("FE-H24H-AM-75", "FE-H24H-AM-75", "FE-H24H-AM", 75,"H24SFEAM75").put();
+		new Mondai("FE-H24H-AM-76", "FE-H24H-AM-76", "FE-H24H-AM", 76,"H24SFEAM76").put();
+		new Mondai("FE-H24H-AM-77", "FE-H24H-AM-77", "FE-H24H-AM", 77,"H24SFEAM77").put();
+		new Mondai("FE-H24H-AM-78", "FE-H24H-AM-78", "FE-H24H-AM", 78,"H24SFEAM78").put();
+		new Mondai("FE-H24H-AM-79", "FE-H24H-AM-79", "FE-H24H-AM", 79,"H24SFEAM79").put();
+		new Mondai("FE-H24H-AM-80", "FE-H24H-AM-80", "FE-H24H-AM", 80,"H24SFEAM80").put();
+		}
+	public static void q() throws NoSuchFieldException {
+		new Question("H24SFEAM01", "平成24年春 基本情報 午前 01", 4, 3).put();
+		new Question("H24SFEAM02", "平成24年春 基本情報 午前 02", 4, 1).put();
+		new Question("H24SFEAM03", "平成24年春 基本情報 午前 03", 4, 4).put();
+		new Question("H24SFEAM04", "平成24年春 基本情報 午前 04", 4, 2).put();
+		new Question("H24SFEAM05", "平成24年春 基本情報 午前 05", 4, 4).put();
+		new Question("H24SFEAM06", "平成24年春 基本情報 午前 06", 4, 2).put();
+		new Question("H24SFEAM07", "平成24年春 基本情報 午前 07", 4, 4).put();
+		new Question("H24SFEAM08", "平成24年春 基本情報 午前 08", 4, 2).put();
+		new Question("H24SFEAM09", "平成24年春 基本情報 午前 09", 4, 3).put();
+		new Question("H24SFEAM10", "平成24年春 基本情報 午前 10", 4, 2).put();
+
+		new Question("H24SFEAM11", "平成24年春 基本情報 午前 11", 4, 1).put();
+		new Question("H24SFEAM12", "平成24年春 基本情報 午前 12", 4, 3).put();
+		new Question("H24SFEAM13", "平成24年春 基本情報 午前 13", 4, 3).put();
+		new Question("H24SFEAM14", "平成24年春 基本情報 午前 14", 4, 1).put();
+		new Question("H24SFEAM15", "平成24年春 基本情報 午前 15", 4, 3).put();
+		new Question("H24SFEAM16", "平成24年春 基本情報 午前 16", 4, 1).put();
+		new Question("H24SFEAM17", "平成24年春 基本情報 午前 17", 4, 2).put();
+		new Question("H24SFEAM18", "平成24年春 基本情報 午前 18", 4, 2).put();
+		new Question("H24SFEAM19", "平成24年春 基本情報 午前 19", 4, 4).put();
+		new Question("H24SFEAM20", "平成24年春 基本情報 午前 20", 4, 3).put();
+
+		new Question("H24SFEAM21", "平成24年春 基本情報 午前 21", 4, 1).put();
+		new Question("H24SFEAM22", "平成24年春 基本情報 午前 22", 4, 2).put();
+		new Question("H24SFEAM23", "平成24年春 基本情報 午前 23", 4, 3).put();
+		new Question("H24SFEAM24", "平成24年春 基本情報 午前 24", 4, 3).put();
+		new Question("H24SFEAM25", "平成24年春 基本情報 午前 25", 4, 3).put();
+		new Question("H24SFEAM26", "平成24年春 基本情報 午前 26", 4, 3).put();
+		new Question("H24SFEAM27", "平成24年春 基本情報 午前 27", 4, 3).put();
+		new Question("H24SFEAM28", "平成24年春 基本情報 午前 28", 4, 2).put();
+		new Question("H24SFEAM29", "平成24年春 基本情報 午前 29", 4, 1).put();
+		new Question("H24SFEAM30", "平成24年春 基本情報 午前 30", 4, 3).put();
+		
+		new Question("H24SFEAM31", "平成24年春 基本情報 午前 31", 4, 3).put();
+		new Question("H24SFEAM32", "平成24年春 基本情報 午前 32", 4, 4).put();
+		new Question("H24SFEAM33", "平成24年春 基本情報 午前 33", 4, 4).put();
+		new Question("H24SFEAM34", "平成24年春 基本情報 午前 34", 4, 4).put();
+		new Question("H24SFEAM35", "平成24年春 基本情報 午前 35", 4, 3).put();
+		new Question("H24SFEAM36", "平成24年春 基本情報 午前 36", 4, 3).put();
+		new Question("H24SFEAM37", "平成24年春 基本情報 午前 37", 4, 3).put();
+		new Question("H24SFEAM38", "平成24年春 基本情報 午前 38", 4, 3).put();
+		new Question("H24SFEAM39", "平成24年春 基本情報 午前 39", 4, 2).put();
+		new Question("H24SFEAM40", "平成24年春 基本情報 午前 40", 4, 2).put();
+		
+		new Question("H24SFEAM41", "平成24年春 基本情報 午前 41", 4, 3).put();
+		new Question("H24SFEAM42", "平成24年春 基本情報 午前 42", 4, 4).put();
+		new Question("H24SFEAM43", "平成24年春 基本情報 午前 43", 4, 1).put();
+		new Question("H24SFEAM44", "平成24年春 基本情報 午前 44", 4, 2).put();
+		new Question("H24SFEAM45", "平成24年春 基本情報 午前 45", 4, 3).put();
+		new Question("H24SFEAM46", "平成24年春 基本情報 午前 46", 4, 1).put();
+		new Question("H24SFEAM47", "平成24年春 基本情報 午前 47", 4, 3).put();
+		new Question("H24SFEAM48", "平成24年春 基本情報 午前 48", 4, 2).put();
+		new Question("H24SFEAM49", "平成24年春 基本情報 午前 49", 4, 1).put();
+		new Question("H24SFEAM50", "平成24年春 基本情報 午前 50", 4, 3).put();
+	
+		new Question("H24SFEAM51", "平成24年春 基本情報 午前 51", 4, 3).put();
+		new Question("H24SFEAM52", "平成24年春 基本情報 午前 52", 4, 3).put();
+		new Question("H24SFEAM53", "平成24年春 基本情報 午前 53", 4, 3).put();
+		new Question("H24SFEAM54", "平成24年春 基本情報 午前 54", 4, 3).put();
+		new Question("H24SFEAM55", "平成24年春 基本情報 午前 55", 4, 1).put();
+		new Question("H24SFEAM56", "平成24年春 基本情報 午前 56", 4, 4).put();
+		new Question("H24SFEAM57", "平成24年春 基本情報 午前 57", 4, 2).put();
+		new Question("H24SFEAM58", "平成24年春 基本情報 午前 58", 4, 2).put();
+		new Question("H24SFEAM59", "平成24年春 基本情報 午前 59", 4, 1).put();
+		new Question("H24SFEAM60", "平成24年春 基本情報 午前 60", 4, 4).put();
+		
+		new Question("H24SFEAM61", "平成24年春 基本情報 午前 61", 4, 2).put();
+		new Question("H24SFEAM62", "平成24年春 基本情報 午前 62", 4, 4).put();
+		new Question("H24SFEAM63", "平成24年春 基本情報 午前 63", 4, 4).put();
+		new Question("H24SFEAM64", "平成24年春 基本情報 午前 64", 4, 4).put();
+		new Question("H24SFEAM65", "平成24年春 基本情報 午前 65", 4, 3).put();
+		new Question("H24SFEAM66", "平成24年春 基本情報 午前 66", 4, 2).put();
+		new Question("H24SFEAM67", "平成24年春 基本情報 午前 67", 4, 1).put();
+		new Question("H24SFEAM68", "平成24年春 基本情報 午前 68", 4, 4).put();
+		new Question("H24SFEAM69", "平成24年春 基本情報 午前 69", 4, 2).put();
+		new Question("H24SFEAM70", "平成24年春 基本情報 午前 70", 4, 2).put();
+		
+		new Question("H24SFEAM71", "平成24年春 基本情報 午前 71", 4, 4).put();
+		new Question("H24SFEAM72", "平成24年春 基本情報 午前 72", 4, 1).put();
+		new Question("H24SFEAM73", "平成24年春 基本情報 午前 73", 4, 4).put();
+		new Question("H24SFEAM74", "平成24年春 基本情報 午前 74", 4, 4).put();
+		new Question("H24SFEAM75", "平成24年春 基本情報 午前 75", 4, 2).put();
+		new Question("H24SFEAM76", "平成24年春 基本情報 午前 76", 4, 2).put();
+		new Question("H24SFEAM77", "平成24年春 基本情報 午前 77", 4, 2).put();
+		new Question("H24SFEAM78", "平成24年春 基本情報 午前 78", 4, 4).put();
+		new Question("H24SFEAM79", "平成24年春 基本情報 午前 79", 4, 2).put();
+		new Question("H24SFEAM80", "平成24年春 基本情報 午前 80", 4, 4).put();
+	}
+}
