7aca9be3fb6e359923b5c1a6c566f7eef30bf517
yoshzawa <yoshzawa@gmail.com>
2017-07-16 09:31:27 +0900

    Azureの定数を保存するようにした

---
+++ src/com/gmail/yoshzawa/AzureOpenId/AzureConstant.java
@@ -0,0 +1,6 @@
+package com.gmail.yoshzawa.AzureOpenId;
+
+public interface AzureConstant {
+	public static final String AzureAppName="h28jk3aopenidtest.appspot.com";
+	public static final String AzureAppId="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
+}
