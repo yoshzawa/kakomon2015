a4f57ecf7932e93643db35699b7ae1068dde14c2
yoshzawa <yoshzawa@gmail.com>
2017-07-17 16:15:59 +0900

    AzureConstraintをgitに登録しないで行きたい

--- src/com/gmail/yoshzawa/AzureOpenId/AzureConstant.java
+++ src/com/gmail/yoshzawa/AzureOpenId/AzureConstant.java
@@ -1,6 +1,8 @@
 package com.gmail.yoshzawa.AzureOpenId;
 
 public interface AzureConstant {
+	// Azureに登録したときのURL
 	public static final String AzureAppName="h28jk3aopenidtest.appspot.com";
+	// 発行されたAppID
 	public static final String AzureAppId="XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX";
 }
